c98139d11b61deae5735d8364bdf30c6ce7c0c55
Duc Nguyen <minhducng93@gmail.com>
2017-02-19 01:23:09 -0500

    Add demo nested URL get request and para

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -12,6 +12,14 @@ drop.get("hello") { request in
     return ("Hello world")
 }
 
+drop.get("hello","vapor") { request in
+    return try JSON(node: ["Greeting":"Hello vapor"])
+}
+
+drop.get("chat", Int.self) { request, chat in
+    return try JSON(node: ["person","person\(chat)"])
+}
+
 drop.resource("posts", PostController())
 
 drop.run()
